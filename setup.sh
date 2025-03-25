c1e49982d18a2e9b5136195a7998458a130a334c
TaYaKi71751 <shantian.tw@gmail.com>
2025-03-25 18:59:22 +0900

    Don\'t show example.org

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ sed -i 's/^Allow 127.0.0.1$/Allow 0.0.0.0\/0/' /etc/tinyproxy/tinyproxy.conf
 echo -e "tinyproxy &\ncat /dev/location > /dev/null &\nps aux | grep tinyproxy | grep -v grep\ngrep '^Allow' /etc/tinyproxy/tinyproxy.conf" >> ~/.profile
 echo "sleep 10" >> ~/.profile
 echo "while true;do" >> ~/.profile
-echo "if ( http_proxy=127.0.0.1:8888 curl example.org );then" >> ~/.profile
+echo "if ( http_proxy=127.0.0.1:8888 curl example.org &> /dev/null > /dev/null );then" >> ~/.profile
 echo "sleep 5" >> ~/.profile
 echo "else" >> ~/.profile
 echo "sleep 5" >> ~/.profile
