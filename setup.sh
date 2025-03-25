6f87fb16933c7126f5873008fa4eb5a525d895fe
TaYaKi71751 <shantian.tw@gmail.com>
2025-03-25 18:51:40 +0900

    Start with delay

--- setup.sh
+++ setup.sh
@@ -3,6 +3,8 @@ apk update
 apk add tinyproxy
 apk add curl
 sed -i 's/^Allow 127.0.0.1$/Allow 0.0.0.0\/0/' /etc/tinyproxy/tinyproxy.conf
+echo -e "tinyproxy &\ncat /dev/location > /dev/null &\nps aux | grep tinyproxy | grep -v grep\ngrep '^Allow' /etc/tinyproxy/tinyproxy.conf" >> ~/.profile
+echo "sleep 10" >> ~/.profile
 echo "while true;do" >> ~/.profile
 echo "if ( http_proxy=127.0.0.1:8888 curl example.org );then" >> ~/.profile
 echo "continue"
