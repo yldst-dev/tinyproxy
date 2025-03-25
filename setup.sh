5c26e409ccd1de7ac6d72511915d13f0f987bb89
TaYaKi71751 <shantian.tw@gmail.com>
2025-03-25 18:53:54 +0900

    Fix typo & missing things

--- setup.sh
+++ setup.sh
@@ -7,8 +7,9 @@ echo -e "tinyproxy &\ncat /dev/location > /dev/null &\nps aux | grep tinyproxy |
 echo "sleep 10" >> ~/.profile
 echo "while true;do" >> ~/.profile
 echo "if ( http_proxy=127.0.0.1:8888 curl example.org );then" >> ~/.profile
-echo "continue"
-echo "else"
+echo "sleep 5" >> ~/.profile
+echo "else" >> ~/.profile
+echo "sleep 5" >> ~/.profile
 echo -e "tinyproxy &\ncat /dev/location > /dev/null &\nps aux | grep tinyproxy | grep -v grep\ngrep '^Allow' /etc/tinyproxy/tinyproxy.conf" >> ~/.profile
 echo "fi" >> ~/.profile
 echo "done" >> ~/.profile
